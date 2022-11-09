b056654912bb93e41e58dcd3d07e4aed794d7d4d
Marvin Martínez <marvin1ronal@gmail.com>
2022-11-09 08:58:43 -0600

    Update master at 11/9/2022 8:58:41 AM

--- sonar/sonar.js
+++ sonar/sonar.js
@@ -4,12 +4,12 @@ scanner(
     {
         serverUrl: 'http://sonar-qb-prod.isoft-ste.com/',
         options: {
-            'sonar.projectName': 'ejemplo-solucredit',
+            'sonar.projectName': 'ejemplo-sidra-pa',
             'sonar.projectVersion': '1.0.0',
             'sonar.projectDescription': 'Backend test qa nodejs',
-            'sonar.sources': '../solucredit-app/src',
+            'sonar.sources': '../solucredit-app-jasper/src',
             'sonar.login': 'd9618251fc7ab20cb7461e22831ddde5ca2050bc',
-            'sonar.projectBaseDir': '../solucredit-app',
+            'sonar.projectBaseDir': '../solucredit-app-jasper',
         }
     },
     () => process.exit()
